3ede4813e916e79eb048e8e1f189add37a2900d4
Zaher Dirkey <zaherdirkey@yahoo.com>
2016-11-08 01:14:36 +0200

    Add Pause, Sleep functions, Pause not freeze

--- main.lua
+++ main.lua
@@ -1,35 +1,42 @@
-require("basic")
-
-draw = {}
+canvas = {}
 
 --load_module = require "main_load"
 --love.load = load_module.load
 
-run_file = arg[2] .. ".lua"
+run_file = arg[#arg]
 run = assert(loadfile(run_file))
+local paused = nil --a end time to finish, and resume run coroutine
 
 function love.load()
--- create canvas
-    canvas = love.graphics.newCanvas()
-    love.graphics.setCanvas(canvas)
-	love.graphics.setColor(255,255,255)
+-- create buffer
+    buffer = love.graphics.newCanvas()
+    love.graphics.setCanvas(buffer)
+    love.graphics.setBlendMode("replace")
+--	love.graphics.setColor(255,255,255)
     love.graphics.setCanvas()
     if run then
-    	print("doing it")
     	co = coroutine.create(run)
-        print(coroutine.status)
         coroutine.resume(co)
-        print(coroutine.status)
     end
 end
 
 function love.draw()
     --love.graphics.setColor(255,255,255)
-    love.graphics.draw(canvas)
+    love.graphics.draw(buffer)
+    if canvas.draw then
+    	canvas.draw()
+    end
+
     if co then
-    	if not coroutine.resume(co) then
-            co = nil
+    	if paused and (paused < os.clock()) then
+        	paused = nil
     	end
+
+        if not paused then
+    	    if not coroutine.resume(co) then
+                co = nil
+    	    end
+        end
 	end
 end
 
@@ -39,7 +46,9 @@ end
 -----------------------------------------------------
 
 function refresh()
-   coroutine.yield()
+    if co then
+		coroutine.yield()
+	end
 end
 
 text = 0
@@ -49,36 +58,46 @@ local screenmode = text
 
 function screen(mode)
   screenmode = mode
+  --todo check if locked
   refresh()
 end
 
-function draw.setcolor(r, b, g)
+function canvas.setcolor(r, b, g)
 	love.graphics.setColor(r, b, g)
-    refresh()
+    --no need to referesh
 end
 
-function draw.circle(x, y, r)
-    love.graphics.setCanvas(canvas)
+function canvas.circle(x, y, r)
+    love.graphics.setCanvas(buffer)
 	love.graphics.circle("line", x, y, r)
     love.graphics.setCanvas()
     refresh()
 end
 
-function draw.rectangle(x, y, size)
-    love.graphics.setCanvas(canvas)
+function canvas.rectangle(x, y, size)
+    love.graphics.setCanvas(buffer)
     love.graphics.rectangle('line',x, y, size, size)
     love.graphics.setCanvas()
     refresh()
 end
 
-function draw.clear()
-    love.graphics.setCanvas(canvas)
+function canvas.clear()
+    love.graphics.setCanvas(buffer)
     love.graphics.clear();
     love.graphics.setCanvas()
     refresh()
 end
 
-function pause(ms)
-    love.timer.sleep(ms)
+--------------------------
+-- Freeze your program
+--------------------------
+
+function sleep(seconds)
+    love.timer.sleep(seconds)
+    --refresh()
+end
+
+function pause(seconds)
+	paused = os.clock() + seconds
     refresh()
 end
